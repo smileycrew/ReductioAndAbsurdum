b31c151e792db978eb5f02189dd788b8f2f8853c
Edwin Moz <edwin.moz@icloud.com>
2023-11-20 10:40:20 -0600

    third commit

--- Program.cs
+++ Program.cs
@@ -2,16 +2,45 @@
 // add a product to the inventory,
 // delete a product from the inventory,
 // and update a product's details
-Console.WriteLine(@"choose one of the following:
-0. end program
-1. view all products
-2. add a product
-3. delete a product
-4. update a product's detail");
 
-string chosenOption;
+using System.Linq.Expressions;
 
+string chosenOption = null;
 
+while (chosenOption != "0")
+{
+    Console.WriteLine(@"choose one of the following:
+    0. end program
+    1. view all products
+    2. add a product
+    3. delete a product
+    4. update a product's detail");
+
+    chosenOption = Console.ReadLine();
+
+    Console.WriteLine($"you chose option {chosenOption}");
+
+    if (chosenOption == "0")
+    {
+        Console.WriteLine("goodbye");
+    }
+    else if (chosenOption == "1")
+    {
+        throw new NotImplementedException("view all plants");
+    }
+    else if (chosenOption == "2")
+    {
+        throw new NotImplementedException("add a product");
+    }
+    else if (chosenOption == "3")
+    {
+        throw new NotImplementedException("delete a product");
+    }
+    else if (chosenOption == "4")
+    {
+        throw new NotImplementedException("update a product's detail");
+    }
+}
 // Products have a name, price (as a decimal), and a boolean to indicate whether they are still available. 
 //They also have a ProductTypeId, which categorize them into the following categories: apparel, potions, enchanted objects, and wands.
 // When adding a product to the inventory, the user should be able to choose from these options to add a product type id to the new product.
@@ -19,4 +48,3 @@ string chosenOption;
 // Product Types have Name and Id properties.
 
 // Red and Abe should be able to look up products in their database by product type.
-Console.WriteLine("Hello, World!");
