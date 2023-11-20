07d9a9947e863362e755369603f357423eae0a17
Edwin Moz <edwin.moz@icloud.com>
2023-11-20 10:24:43 -0600

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Create a main menu with working options to view all products,
+// add a product to the inventory,
+// delete a product from the inventory,
+// and update a product's details
+Console.WriteLine(@"choose one of the following:
+0. end program
+1. view all products
+2. add a product
+3. delete a product
+4. update a product's detail");
+
+string chosenOption;
+
+
+// Products have a name, price (as a decimal), and a boolean to indicate whether they are still available. 
+//They also have a ProductTypeId, which categorize them into the following categories: apparel, potions, enchanted objects, and wands.
+// When adding a product to the inventory, the user should be able to choose from these options to add a product type id to the new product.
+
+// Product Types have Name and Id properties.
+
+// Red and Abe should be able to look up products in their database by product type.
 Console.WriteLine("Hello, World!");
